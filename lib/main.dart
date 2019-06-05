94e742538803089ce3a0167f11945e25e85890da
Jesse Belcourt <jessebelcourt@jessebelcourt.local>
2019-06-05 09:22:10 -0600

    Removed unnecessary import statements from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
 import 'package:blog_parser/src/conversion_utilities/conversion_engine.dart';
-import 'package:blog_parser/src/conversion_utilities/custom_components.dart';
 
 void main() {
   runApp(ArticleViewApp());
